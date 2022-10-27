f6ba889274ccad27a74477bcd8ff7597b6210223
uzakotim <timauzakov@gmail.com>
2022-10-27 16:59:28 +0200

    Minsowsky easy

--- MinkowskySum/main.cpp
+++ MinkowskySum/main.cpp
@@ -19,9 +19,14 @@ std::vector<Eigen::Vector3d> createMinkowskyPoints(Eigen::Vector3d input,double 
                                                             {-1, 1, 0},   {-1, 1, 1},  {0, -1, -1}, {0, -1, 0},  {0, -1, 1}, {0, 0, -1}, {0, 0, 1},
                                                             {0, 1, -1},   {0, 1, 0},   {0, 1, 1},   {1, -1, -1}, {1, -1, 0}, {1, -1, 1}, {1, 0, -1},
                                                             {1, 0, 0},    {1, 0, 1},   {1, 1, -1},  {1, 1, 0},   {1, 1, 1}};
+    
+    std::vector< std::vector<int> > EXPANSION_DIRECTIONS_EASY = {{-1, -1, -1}, {-1, -1, 1},
+                                                                {-1, 1, -1},{-1, 1, 1},   
+                                                                {1, -1, -1},{1, -1, 1}, 
+                                                                {1, 1, -1},{1, 1, 1}};
     std::vector<Eigen::Vector3d> points;
     points.push_back(input);
-    for (auto vertex: EXPANSION_DIRECTIONS)
+    for (auto vertex: EXPANSION_DIRECTIONS_EASY)
     {
         Eigen::Vector3d point;
         point << input(0)+resolution*vertex[0],input(1)+resolution*vertex[1], input(2)+resolution*vertex[2];
@@ -48,60 +53,79 @@ void execute_tests()
                                                             {-1, 1, 0},   {-1, 1, 1},  {0, -1, -1}, {0, -1, 0},  {0, -1, 1}, {0, 0, -1}, {0, 0, 1},
                                                             {0, 1, -1},   {0, 1, 0},   {0, 1, 1},   {1, -1, -1}, {1, -1, 0}, {1, -1, 1}, {1, 0, -1},
                                                             {1, 0, 0},    {1, 0, 1},   {1, 1, -1},  {1, 1, 0},   {1, 1, 1}};
+    
+    std::vector< std::vector<int> > EXPANSION_DIRECTIONS_EASY = {{-1, -1, -1}, {-1, -1, 1},
+                                                                {-1, 1, -1},{-1, 1, 1},   
+                                                                {1, -1, -1},{1, -1, 1}, 
+                                                                {1, 1, -1},{1, 1, 1}};
     // handleTest(test number, input, reference)
     Eigen::Vector3d input(3);
     double resolution = 1.0;
     input << 0,0,0;
     
     std::vector<Eigen::Vector3d> reference_points;
-    reference_points.push_back(input);
-    for (auto exp_vec : EXPANSION_DIRECTIONS)
-    {
-        Eigen::Vector3d point(3);
-        point << exp_vec[0],exp_vec[1],exp_vec[2];
-        reference_points.push_back(point);    
-    } 
-    handleTest(1,input, resolution,reference_points);
+    // reference_points.push_back(input);
+    // for (auto exp_vec : EXPANSION_DIRECTIONS)
+    // {
+    //     Eigen::Vector3d point(3);
+    //     point << exp_vec[0],exp_vec[1],exp_vec[2];
+    //     reference_points.push_back(point);    
+    // } 
+    // handleTest(1,input, resolution,reference_points);
+    // // handleTest(test number, input, reference)
+    // resolution = 0.6;
+    // input << 0,0,0;
+    
+    // reference_points = {};
+    // reference_points.push_back(input);
+    // for (auto exp_vec : EXPANSION_DIRECTIONS)
+    // {
+    //     Eigen::Vector3d point(3);
+    //     point << exp_vec[0],exp_vec[1],exp_vec[2];
+    //     reference_points.push_back(resolution*point);    
+    // } 
+    // handleTest(2,input, resolution,reference_points);
+
+    // // handleTest(test number, input, reference)
+    // resolution = 1.0;
+    // input << 1,1,1;
+    
+    // reference_points = {};
+    // reference_points.push_back(input);
+    // for (auto exp_vec : EXPANSION_DIRECTIONS)
+    // {
+    //     Eigen::Vector3d point(3);
+    //     point << input(0)+ resolution*exp_vec[0],input(1)+ resolution*exp_vec[1],input(2)+ resolution*exp_vec[2];
+    //     reference_points.push_back(point);    
+    // } 
+    // handleTest(3,input, resolution,reference_points);
+    // // handleTest(test number, input, reference)
+    // resolution = 0.6;
+    // input << -0.5,-2.0,1;
+    
+    // reference_points = {};
+    // reference_points.push_back(input);
+    // for (auto exp_vec : EXPANSION_DIRECTIONS)
+    // {
+    //     Eigen::Vector3d point(3);
+    //     point << input(0)+ resolution*exp_vec[0],input(1)+ resolution*exp_vec[1],input(2)+ resolution*exp_vec[2];
+    //     reference_points.push_back(point);    
+    // } 
+    // handleTest(4,input, resolution,reference_points);
+
     // handleTest(test number, input, reference)
-    resolution = 0.6;
+    resolution = 4.0;
     input << 0,0,0;
     
     reference_points = {};
     reference_points.push_back(input);
-    for (auto exp_vec : EXPANSION_DIRECTIONS)
-    {
-        Eigen::Vector3d point(3);
-        point << exp_vec[0],exp_vec[1],exp_vec[2];
-        reference_points.push_back(resolution*point);    
-    } 
-    handleTest(2,input, resolution,reference_points);
-
-    // handleTest(test number, input, reference)
-    resolution = 1.0;
-    input << 1,1,1;
-    
-    reference_points = {};
-    reference_points.push_back(input);
-    for (auto exp_vec : EXPANSION_DIRECTIONS)
+    for (auto exp_vec : EXPANSION_DIRECTIONS_EASY)
     {
         Eigen::Vector3d point(3);
         point << input(0)+ resolution*exp_vec[0],input(1)+ resolution*exp_vec[1],input(2)+ resolution*exp_vec[2];
         reference_points.push_back(point);    
     } 
-    handleTest(3,input, resolution,reference_points);
-    // handleTest(test number, input, reference)
-    resolution = 0.6;
-    input << -0.5,-2.0,1;
-    
-    reference_points = {};
-    reference_points.push_back(input);
-    for (auto exp_vec : EXPANSION_DIRECTIONS)
-    {
-        Eigen::Vector3d point(3);
-        point << input(0)+ resolution*exp_vec[0],input(1)+ resolution*exp_vec[1],input(2)+ resolution*exp_vec[2];
-        reference_points.push_back(point);    
-    } 
-    handleTest(4,input, resolution,reference_points);
+    handleTest(5,input, resolution,reference_points);
 }
 
 /* ----------------------------------------------------- */
